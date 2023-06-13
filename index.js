c73dbebdfccc17facb0889906396690dfe19dce2
user <mutukukennedy34@gmail.com>
2023-06-13 12:21:59 +0300

    Controlflow

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <= 400){
+    return "This one is on me!"
+  } else if(distance>2000 && distance<2500){
+    return 'I will gladly take your thirty bucks.'
+  }else if(distance>2500 ){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const checkcity = (city == 'NYC')? "Ok, sounds good." : "No go."
+  return checkcity;
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch (tip) {
+    case 'generous':
+      message ='Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break
+    default:
+      return message = 'Bye.'
+  }
+  return message;
 }
